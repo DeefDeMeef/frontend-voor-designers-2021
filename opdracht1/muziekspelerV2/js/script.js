27dcc598598b21f035440c4c261dd4a68dc0fad2
Davey <30734726+DeefDeMeef@users.noreply.github.com>
2021-02-09 14:26:52 +0100

    Add files via upload

---
+++ opdracht1/muziekspelerV2/js/script.js
@@ -0,0 +1,14 @@
+let root = document.documentElement
+
+var draggables = document.querySelectorAll('.draggable')
+var containers = document.querySelectorAll('.container')
+
+draggables.forEach(draggable => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging')
+  })
+
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging')
+  })
+})
