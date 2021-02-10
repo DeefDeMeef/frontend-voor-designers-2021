520a351d1b80498c82ff76e80eadd56a9ef1f930
Davey <30734726+DaveyZaal@users.noreply.github.com>
2021-02-10 13:12:47 +0100

    testen

--- opdracht1/muziekspelerV2/js/script.js
+++ opdracht1/muziekspelerV2/js/script.js
@@ -1,7 +1,7 @@
 let root = document.documentElement
 
-var draggables = document.querySelectorAll('.draggable')
-var containers = document.querySelectorAll('.container')
+const draggables = document.querySelectorAll('.draggable')
+const containers = document.querySelectorAll('.container')
 
 draggables.forEach(draggable => {
   draggable.addEventListener('dragstart', () => {
@@ -12,3 +12,30 @@ draggables.forEach(draggable => {
     draggable.classList.remove('dragging')
   })
 })
+
+containers.forEach(container => {
+  container.addEventListener('dragover', e => {
+    e.preventDefault() // standaard gedrag van dit element pas je aan.
+    const afterElement = getDragAfterElement(container, e.clientY)
+    const draggable = document.querySelector('.dragging')
+    if (afterElement == null) {
+      container.appendChild(draggable)
+    } else {
+      container.insertBefore(draggable, afterElement)
+    }
+  })
+})
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect()
+    const offset = y - box.top - box.height / 2
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child } // offset: en child: word terug gegeven aan afterElement
+    } else {
+      return closest
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element
+}
